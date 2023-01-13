8cdd08ae22509243ec1c543877f2bba07ad94b8e
周行 <793745175@qq.com>
2023-01-13 20:42:33 +0800

    refactor: code refactoring

--- router_handler/artcate.js
+++ router_handler/artcate.js
@@ -15,8 +15,8 @@ exports.addArticleCates = (req, res) => {
     const { name, alias } = body
     db.query('select * from ev_article_cate where name=? or alias=?', [name, alias], (err, results) => {
         if (err) return res.cc(err)
-        if (results.length === 2) return res.cc('分类名称和别名被占用，请更换后重试！')
-        if (results.length === 1 && results[0].name === name && results[0].alias === alias) return res.cc('分类名称和别名被占用，请更换后重试！')
+        if (results.length === 2 || (results.length === 1 && results[0].name === name && results[0].alias === alias))
+            return res.cc('分类名称和别名被占用，请更换后重试！')
         if (results.length === 1 && results[0].name === name) return res.cc('分类名称被占用，请更换后重试！')
         if (results.length === 1 && results[0].alias === alias) return res.cc('分类别名被占用，请更换后重试！')
         db.query('insert into ev_article_cate set ?', body, (err, results) => {
@@ -49,8 +49,8 @@ exports.updateArtCateById = (req, res) => {
     const { name, alias, id } = body
     db.query('select * from ev_article_cate where id<>? and (name=? or alias=?)', [id, name, alias], (err, results) => {
         if (err) return res.cc(err)
-        if (results.length === 2) return res.cc('分类名称和别名被占用，请更换后重试！')
-        if (results.length === 1 && results[0].name === name && results[0].alias === alias) return res.cc('分类名称和别名被占用，请更换后重试！')
+        if (results.length === 2 || (results.length === 1 && results[0].name === name && results[0].alias === alias))
+            return res.cc('分类名称和别名被占用，请更换后重试！')
         if (results.length === 1 && results[0].name === name) return res.cc('分类名称被占用，请更换后重试！')
         if (results.length === 1 && results[0].alias === alias) return res.cc('分类别名被占用，请更换后重试！')
         db.query('update ev_article_cate set ? where id=?', [body, id], (err, results) => {
